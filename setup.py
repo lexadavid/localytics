a9add8ea329182eac858b075fdee0ae55dd3e3f4
lexadavid <david@wunder.org>
2018-06-19 13:25:29 +0200

    raw_data_export
    first version of library supporting Raw Data Export

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='localytics',
+    url='https://github.com/lexadavid/localytics',
+    author='David Lexa',
+    author_email='david.kaur.lexa@wunder.org',
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version='1.3',
+    license='MIT',
+    description='Python API Client for Localytics Raw Data Export'
+)
